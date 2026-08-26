3df46ce62ff53a14f810553306b18e1e850d5052
MoralesGuillermo <morales.guillermo2503@gmail.com>
2026-08-26 11:03:52 -0600

    Added s3 object metadata retrieval

--- addMetadataDynamo/add_metadata_dynamo.py
+++ addMetadataDynamo/add_metadata_dynamo.py
@@ -1,13 +1,32 @@
 import json
+import logging
 import boto3
+
+
+s3 = boto3.client("s3")
+dynamo = boto3.client("dynamodb")
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
 
 def lambda_handler(event, context):
     """
-    TO-DO
+    Add uploaded courses metadata to DynamoDB
     """
-        return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "hello": "world"
-        }),
-    }
+    record = event["Records"][0]
+    bucket = record["bucket"]["name"]
+    object_key = record["object"]["key"]
+    object_size = record["object"]["size"]
+
+    try:       
+        # Retrieve objects metadata
+        object_head = s3.head_object(
+            Bucket=bucket,
+            Key=object_key
+        )
+        metadata = object_head["Metadata"]
+    except Exception as e:
+        logger.exception("Object metadata couldn't be retrieved. Trace:")
+        raise 
+
+    # TODO: Add the object's metadata to DynamoDB
